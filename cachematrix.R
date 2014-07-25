be90162f36daa17d2c9d1ce844fcb6909009c839
Rafal Bachorz <rafal@bachorz.eu>
2014-07-25 07:02:39 +0200

    polishing

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below can be used for the calculation of the inverse of the invertible matrix
+## Due to the fact that the calculation of the invertion is quite costly 
+## (scales like n^3, where n is a size of dimension of the matrix) it is a good idea to compute it 
+## once and then access whenever needed.
 
-## Write a short comment describing this function
+## makeCache matrix function creates a special object (cachedValue) that can be accessed 
+## (read or modified) from a different environment.
 makeCacheMatrix <- function(x = matrix()) {
   cachedValue <- NULL
   set <- function(y) {
@@ -17,22 +20,25 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function actually computes the inverse, but only if there is no chached solution.
+## In case when the cachedValue is already computed, it returns that value. In the other case 
+## (the cachedValue is empty; null) it attempts to get the inverse by using the solve function of R.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
   cachedValue <- x$getInverse()
   if(!is.null(cachedValue)) {
     message("getting cached data")
+    ## Return a matrix that is the inverse of 'x' (cached value)
     return(cachedValue)
   }
   data <- x$get()
-  #cachedValue <- mean(data, ...)
   cachedValue <- solve(data, ...)
   x$setInverse(cachedValue)
+  ## Return a matrix that is the inverse of 'x' (freshly computed value)
+  message("computing data")
   cachedValue
 }
-sampleVector=rnorm(400)
-rm(commands, m, xx)
+sampleVector=rnorm(144)
+rm(commands, m, xx, cachedValue)
 
 xx<-matrix(sampleVector, nrow=20, ncol=20)
 commands<-makeCacheMatrix(xx)
